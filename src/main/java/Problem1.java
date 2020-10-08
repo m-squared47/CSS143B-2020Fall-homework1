7c950a4b612e9ee43694e4f5e436c932b64af0db
Martin Matsudaira <mmats23@uw.edu>
2020-10-08 14:26:03 -0700

    Second Commit (complete)

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -2,7 +2,18 @@ public class Problem1 {
 
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
-        // Add your solution
+        //track the index position in the array
+        int index = 0;
+
+        //for every int element in the array
+        for(int integer : data){
+            if(target == integer){
+                return index;
+            }else{
+                index++;
+            }
+        }
         return -1; // Place holder
+        //tells that the array does not contain the target
     }
 }
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,7 +1,30 @@
 public class Problem2 {
 
     // Do not change signature (function name, parameters)
-    public static void bubbleSort(int[] data) {
-        // Add your solution
+    public static int[] bubbleSort(int[] data) {
+        // ascending order
+        int[] toSort = data;        //return array
+        boolean sorted = false;     //loop condition
+        int a;                      //hold 2 values to swap values if needed
+        int b;
+
+        if(data.length >= 2) {      //if there is more than 1 element
+            while (!sorted) {       //if not sorted and the array has more than one element
+                sorted = true;      //tells the loop to run if unsorted. Will remain true if sorted
+
+                for (int i = 0; i < data.length - 1; i++) {
+                    a = toSort[i];
+                    b = toSort[i + 1];
+
+                    if (a > b) {              //if the start element is more than than the next
+                        toSort[i] = b;        //swap
+                        toSort[i + 1] = a;
+                        sorted = false;     //tells the while loop to run again since it's unsorted
+                    }
+                }
+            }
+        }
+
+        return toSort;  //returns the sorted array or the input array if there are 1 or no elements
     }
 }
